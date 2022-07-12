7eb77b6f58eb65e783efe7644648e4a0b4490e3b
Tarmizi Sanusi <realtarmizisanusi@gmail.com>
2022-07-13 01:36:58 +0800

    feat: add custom config

---
+++ src/config/isms.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'username'  =>  env('ISMS_USERNAME', 'medkad'),
+    'password'  =>  env('ISMS_PASSWORD', 'password'),
+    'url'   =>  env('ISMS_URL', 'https://www.isms.com.my/RESTAPI.php'),
+    
+];
